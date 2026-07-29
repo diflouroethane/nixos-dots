4a58f00f2c5b1a1559cc94e1f84e84abf35a5cbe
Ethan Francis <ethanfrancis577@gmail.com>
2026-07-29 11:32:27 -0400

    initial commit! gnome works and i have paperwm and an application launcher too

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+	description = "A test NixOS flake";
+	inputs = {
+ 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+		nixpkgsold.url = "github:NixOS/nixpkgs?rev=7525d999cd850b9a488817abc89c75dc733acf17";
+		#noctalia = {
+		#	url = "github:noctalia-dev/noctalia";
+		#	inputs.nixpkgs.follows = "nixpkgs";
+		#};
+		home-manager = {
+			url = "github:nix-community/home-manager/release-26.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+
+	};
+
+	outputs = {self, nixpkgs,nixpkgsold, home-manager,...}@inputs: {
+		nixosConfigurations.inspiron = nixpkgs.lib.nixosSystem {
+			modules = [
+				./configuration.nix
+				
+				home-manager.nixosModules.home-manager
+				{
+					home-manager.useGlobalPkgs = true;
+					home-manager.useUserPackages = true;
+					home-manager.extraSpecialArgs = {inherit inputs;};
+					home-manager.users.ethan = import ./home.nix;
+				}
+			];
+		};
+		
+	};
+}
---
+++ home.nix
@@ -0,0 +1,94 @@
+{config, pkgs, inputs, ...}: let
+
+pkgsold = inputs.nixpkgsold.legacyPackages.x86_64-linux;
+
+in {
+  home.username = "ethan";
+  home.homeDirectory = "/home/ethan";
+  
+  home.packages = with pkgs; [
+    fastfetch
+    nnn
+    gnomeExtensions.paperwm
+    #swaybg
+    #xwayland-satellite    
+    #font-awesome    
+    pkgsold.gnomeExtensions.raccoon-launcher
+    neovim
+
+    localsend
+    godot
+
+    zip
+    xz
+    unzip
+    
+    pciutils
+    usbutils
+  ];
+
+  imports = [
+    #inputs.noctalia.homeModules.default
+  ];
+
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty;
+    enableBashIntegration = true;
+  };
+  
+  dconf.settings."org/gnome/shell" = {
+    disable-user-extensions = false;
+    enabled-extensions = with pkgs.gnomeExtensions; [
+      paperwm.extensionUuid
+      pkgsold.gnomeExtensions.raccoon-launcher.extensionUuid
+    ];
+  };
+
+#  programs.noctalia = {
+#    enable = true;
+#
+#    settings = {
+#      theme = {
+#       mode = "dark";
+#       source = "builtin";
+#       builtin = "Catppuccin";
+#      };    
+#    };
+#  };
+
+  programs.git = {
+    enable = true;
+    userName = "Ethan Francis";
+    userEmail = "ethanfrancis577@gmail.com";
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+        aws.disabled = true;
+        gcloud.disabled = true;
+    };
+  };
+
+ # programs.noctalia = {
+ #   enable = true;
+#  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+# FOR NIRI
+#  programs.alacritty.enable = true;
+#  programs.fuzzel.enable = true;
+#  programs.swaylock.enable = true;
+#  programs.waybar.enable = true;
+#  services.mako.enable = true;
+#  services.swayidle.enable = true;
+#  services.polkit-gnome.enable = true;
+
+
+  home.stateVersion = "26.05";
+}
