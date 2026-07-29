ae2cc21d0641501be63d3d44ee94b0b0662d64e8
Ethan Francis <ethanfrancis577@gmail.com>
2026-07-29 11:56:16 -0400

    changed timezone, silly me

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,7 @@
 
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "America/New_York";
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
